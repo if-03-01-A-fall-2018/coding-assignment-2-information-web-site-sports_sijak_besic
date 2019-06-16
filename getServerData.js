34d1311ea499eeaebc37550f6cd23fab3ecfceb5
zbenii <besic.beni@gmx.at>
2019-06-16 18:25:38 +0200

    perfected some java code

--- getServerData.js
+++ getServerData.js
@@ -1,4 +1,3 @@
-
 function GetPlayers(){
   var myInit = { method: 'GET',
                  headers: {'Accept': 'application/json','Content-Type': 'application/json'},
@@ -11,18 +10,39 @@ function GetPlayers(){
       return response.json();
     })
     .then(data => {
-      WriteHtml(data);
+      //Methods run with the data
+      var sortedData=SortPlayers(data);
+      WriteHtml(sortedData);
     })
+}
+
+function SortPlayers(data){
+  //Parsing the data so that it's usable
+  var ix=JSON.stringify(data);
+  var obj=JSON.parse(ix);
+
+  var i;
+  var j;
+  for (i = 0; i < data.length; i++) {
+       for(j = 0 ; j < data.length - i-1; j++){
+       if (data[j].points < data[j + 1].points) {
+         var temp = data[j];
+         data[j] = data[j+1];
+         data[j + 1] = temp;
+       }
+      }
+     }
+
+   return data;
 }
 
 function WriteHtml(data){
   var p = document.getElementById("players");
   var i;
+
+  //Adds a line for every player in the table
   for(i=0;i<data.length;i++)
   {
-    var ix=JSON.stringify(data[i]);
-    var obj=JSON.parse(ix);
-
-    p.innerHTML+='<tr>'+'<td class="text-left">'+obj.name+'</td>'+'<td class="text-left">'+obj.points+'</td>'+'</tr>';
+    p.innerHTML+='<tr>'+'<td class="text-left">'+data[i].name+'</td>'+'<td class="text-left">'+data[i].points+'</td>'+'</tr>';
   }
 }
